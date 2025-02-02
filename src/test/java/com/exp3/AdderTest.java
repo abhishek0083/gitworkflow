453615efa39723109908aff04994161d5dd07e08
abhis <abhisharma141099@gmail.com>
2025-02-02 19:39:09 +0530

    update file

--- src/test/java/com/exp3/AdderTest.java
+++ src/test/java/com/exp3/AdderTest.java
@@ -8,10 +8,13 @@ public class AdderTest {
     @Test
     public void testAddition() {
         assertEquals(5, add(2, 3));
-    }
+    }  // Closing brace for the method
 
     private int add(int a, int b) {
         return a + b;
     }
-}
 
+    private void assertEquals(int i, int add) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+}
