ea53515a57f7b2af9ca52746b68b00e17c1a57b7
abhis <abhisharma141099@gmail.com>
2025-02-02 19:33:06 +0530

    update file

--- src/test/java/com/exp3/AdderTest.java
+++ src/test/java/com/exp3/AdderTest.java
@@ -1,19 +1,3 @@
-// package com.exp3;
-
-// import static org.junit.Assert.assertEquals;
-// import org.junit.Test;
-
-// public class AdderTest {
-
-//     @Test
-//     public void testAddition() {
-//         assertEquals(5, add(2, 3));
-//     }
-
-//     private int add(int a, int b) {
-//         return a + b;
-//     }
-// }
 package com.exp3;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
