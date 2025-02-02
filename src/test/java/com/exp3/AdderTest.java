25dd151a7385fc4cbf28e5f94e118003bf2aa458
abhis <abhisharma141099@gmail.com>
2025-02-02 20:19:00 +0530

    update file

--- src/test/java/com/exp3/AdderTest.java
+++ src/test/java/com/exp3/AdderTest.java
@@ -1,12 +1,12 @@
 package com.exp3;
 
-import static org.junit.Assert.assertEquals;
-import org.junit.Test;
+import static org.junit.Assert.assertEquals; // JUnit 4 import
+import org.junit.Test; // JUnit 4 import
 
-public class AdderTest{
+public class AdderTest {
 
     @Test
-    public void testAddition() {
+    public void Adder() {
         assertEquals(5, add(2, 3));
     }
 
